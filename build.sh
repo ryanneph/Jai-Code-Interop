71ba8f2d8c73cfadac5aa066b0a788942a2aaa86
Ryan Neph <neph320@gmail.com>
2022-03-24 23:03:13 -0700

    jai executable builder

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -euo pipefail
 
 OUT="./build_output"
 mkdir -p $OUT
+
+# BUILD libasm.o
+nasm -f elf64 -o $OUT/libasm.a lib.asm
 
 # BUILD libjai.a
 touch "$OUT/libjai.a" # TODO(ryan): remove after jai-linux update
 jai builder.jai
 
-# BUILD libasm.o
-nasm -f elf64 -o $OUT/libasm.o lib.asm
-
 # BUILD main_c (link w/ libjai and libasm)
 g++ -static -g -o $OUT/main_c \
-  main.cpp $OUT/libasm.o $OUT/libjai.a -lpthread
+  main.cpp $OUT/libasm.a $OUT/libjai.a -lpthread
