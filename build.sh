65daf253f41999270cee3f09fb2b113dcaf19602
Ryan Neph <neph320@gmail.com>
2022-03-30 12:42:14 -0700

    call libc::cpp_add from asm_main

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ mkdir -p $OUT
 # BUILD libasm.a - need to use .a extension to make jai's #foreign_lib happy
 nasm -f elf64 -o $OUT/libasm.a lib.asm
 
-# BUILD main_asm (link w/ libasm)
-nasm -f elf64 -o $OUT/main_asm.o main.asm
-ld -o $OUT/main_asm $OUT/main_asm.o $OUT/libasm.a
-
 # BUILD libc.o - need to use .a extension to make jai's #foreign_lib happy
 g++ -c -o $OUT/libcpp.a lib.cpp
+
+# BUILD main_asm (link w/ libasm)
+nasm -f elf64 -o $OUT/main_asm.o main.asm
+ld -o $OUT/main_asm $OUT/main_asm.o $OUT/libasm.a $OUT/libcpp.a
 
 # BUILD libjai.a and main_jai
 touch "$OUT/libjai.a" # TODO(ryan): remove after jai-linux update
--- main.asm
+++ main.asm
@@ -1,10 +1,21 @@
 section .data
     str_hello: db "hello from asm-land",10
     len_hello: equ $-str_hello
+    str_prefix: db "asm: "
+    len_prefix: equ $-str_prefix
+    str_line_feed: db 10
+    len_line_feed: equ 1
+    str_asm_did: db "asm did "
+    len_asm_did: equ $-str_asm_did
+    str_cpp_did: db "cpp did "
+    len_cpp_did: equ $-str_cpp_did
+    str_jai_did: db "jai did "
+    len_jai_did: equ $-str_jai_did
 
 section .text
     global _start
     extern asm_add
+    extern cpp_add
 
 sys_write:
     ;; caller provides:
@@ -13,6 +24,73 @@ sys_write:
     mov rax,1 ; write()
     mov rdi,1 ; stdout
     syscall
+    ret
+
+print_line_feed:
+    mov rsi,str_line_feed
+    mov rdx,len_line_feed
+    call sys_write
+    ret
+
+print_single_digit:
+    ; print a number in [0, 9] as char. will choke for numbers out-of-range.
+    ;; caller provides:
+    ; edi - num
+    add edi,48 ; convert integer result to char
+
+    ; make some space on the stack to store our char (can probably use the
+    ;   red-zone for this instead to save some instructions)
+    sub rsp,1
+    mov byte [rsp],dil ; output digit - lower byte of edi
+    mov rsi,rsp
+    mov rdx,1 ; string length
+    call sys_write
+    add rsp,1 ; cleanup our use of the stack
+    ret
+
+cpp_add_and_print:
+    ;; call cpp_add from our libc
+    ; first print the result with some flavor text
+    mov rsi,str_prefix
+    mov rdx,len_prefix
+    call sys_write ; write "asm: "
+
+    mov rsi,str_cpp_did
+    mov rdx,len_cpp_did
+    call sys_write ; write "cpp_did "
+
+    mov edi,4
+    call print_single_digit
+
+    ; hack to print a string as chars ' + '
+    ; TODO build string on stack, instead?
+    mov edi,-16
+    call print_single_digit
+    mov edi,-5
+    call print_single_digit
+    mov edi,-16
+    call print_single_digit
+
+    mov edi,3
+    call print_single_digit
+
+    ; hack to print a string as chars ' = '
+    ; TODO build string on stack, instead?
+    mov edi,-16
+    call print_single_digit
+    mov edi,13
+    call print_single_digit
+    mov edi,-16
+    call print_single_digit
+
+    ; a + b
+    mov edi,4 ; param "a" (s32)
+    mov esi,3 ; param "b" (s32)
+    call cpp_add
+    ; print resulting digit
+    mov edi,eax
+    call print_single_digit
+    call print_line_feed
     ret
 
 _start:
@@ -28,15 +106,11 @@ _start:
     call asm_add
 
     ; build a string containing the digit then print the answer
-    add rax,48 ; convert integer result to char
-    ; make space on the stack
-    mov byte [rsp-2],al ; output digit - lower byte of rax
-    mov byte [rsp-1],10 ; linefeed
-    sub rsp,2
-    mov rsi,rsp
-    mov rdx,2 ; string length
-    call sys_write
-    add rsp,2 ; cleanup our use of the stack
+    mov edi,eax
+    call print_single_digit
+    call print_line_feed
+
+    call cpp_add_and_print
 
     ; exit(s32 error_code)
     mov rax,60 ; exit()
