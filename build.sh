9498fab9373fa02ea8f8ca06569c166d148adf17
Ryan Neph <neph320@gmail.com>
2022-03-30 23:23:24 -0700

    build lib.jai and main_jai separately

--- build.sh
+++ build.sh
@@ -4,29 +4,43 @@ set -euo pipefail
 OUT="./build_output"
 mkdir -p $OUT
 
+###################
+# BUILD LIBRARIES #
+###################
 # BUILD libasm.a - need to use .a extension to make jai's #foreign_lib happy
 nasm -f elf64 -o $OUT/libasm.a lib.asm
 
 # BUILD libc.o - need to use .a extension to make jai's #foreign_lib happy
 g++ -c -o $OUT/libcpp.a lib.cpp
 
+# BUILD libjai.a
+touch "$OUT/libjai.a" # TODO(ryan): remove after jai-linux update
+jai builder.jai -- -build-lib
+
+
+#####################
+# BUILD EXECUTABLES #
+#####################
 # BUILD main_asm (link w/ libasm)
 nasm -f elf64 -o $OUT/main_asm.o main.asm
 ld -o $OUT/main_asm $OUT/main_asm.o $OUT/libasm.a $OUT/libcpp.a
-
-# BUILD libjai.a and main_jai
-touch "$OUT/libjai.a" # TODO(ryan): remove after jai-linux update
-jai builder.jai
 
 # BUILD main_c (link w/ libjai and libasm)
 g++ -static -g -o $OUT/main_cpp \
   main.cpp $OUT/libasm.a $OUT/libjai.a -lpthread
 
+# BUILD main_jai
+jai builder.jai -- -build-exe
+
+
+###################
+# RUN EXECUTABLES #
+###################
 if [[ "${1:-}" == 'test' ]]; then
   echo -e "\n=========================="
   echo -e   "          TESTS           "
   echo -e   "=========================="
-  echo -e "\nmain_asm:"
+  echo -e "main_asm:"
   $OUT/main_asm
   echo -e "\nmain_cpp:"
   $OUT/main_cpp
